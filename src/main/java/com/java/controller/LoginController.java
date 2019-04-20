347071dd402a0a7537268c872f2f35a4e18a250f
jtran64 <jtran416069@gmail.com>
2019-04-20 16:41:30 -0500

    updating controllers

--- src/main/java/com/java/controller/LoginController.java
+++ src/main/java/com/java/controller/LoginController.java
@@ -7,7 +7,9 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,15 +18,25 @@ import com.java.dto.User;
 import com.java.service.LoginService;
 
 @Controller
+@CrossOrigin(origins="http://localhost:4200")
 public class LoginController {
 	@Autowired
-	LoginService service;
+	private LoginService service;
 
 	@PostMapping("/login.do")
-	public String login(HttpServletRequest request, HttpServletResponse response, @RequestParam String username, @RequestParam String password) {
-		User user = null;
-		user = service.login(username, password);
-		if (user != null && user.getUsername().equals(username) && user.getPassword().equals(password)) {
+	public String login(HttpServletRequest request, HttpServletResponse response, @ModelAttribute User user/*@RequestParam String username, @RequestParam String password*/) {
+		/*User user = null;
+		user = new User();
+		user.setUsername(username);
+		user.setPassword(password);*/
+		//user = service.login(username, password);
+		System.out.println(user);
+		if (user != null && "jtran".equals(user.getUsername()) && "pass".equals(user.getPassword())) {
+			System.out.println("Login successful");
+			request.getSession().setAttribute("User", user);
+			return "home";
+		}
+		/*if (user != null && user.getUsername().equals(username) && user.getPassword().equals(password)) {
 			request.getSession().setAttribute("User", user);
 			return "forward:/home";
 		}else {
@@ -34,7 +46,7 @@ public class LoginController {
 				
 				e.printStackTrace();
 			}
-		}
+		}*/
 		return null;
 	}
 	
